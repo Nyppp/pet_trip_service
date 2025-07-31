10b47c3fa3c809fe011b09da78134f3e9243444e
Nyppp <wlsdnrla216@naver.com>
2025-07-31 13:26:23 +0900

    feat: javadoc 주석 추가 및 스케쥴 < > 아이템 관계 설정

--- src/main/java/com/oreumi/pet_trip_service/model/Schedule.java
+++ src/main/java/com/oreumi/pet_trip_service/model/Schedule.java
@@ -2,16 +2,35 @@ package com.oreumi.pet_trip_service.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * 스케쥴(일정) 엔티티에 대한 모델 클래스
+ * <h2>엔티티 구성 요소</h2>
+ * <ul>
+ *     <li>고유 ID</li>
+ *     <li>유저 정보(스케쥴 제작자)</li>
+ *     <li>유저 정보(참가자)</li>
+ *     <li>스케쥴 아이템</li>
+ *     <li>일정</li>
+ *     <li>생성 시각</li>
+ * </ul>
+ */
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Schedule {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,6 +47,9 @@ public class Schedule {
     )
     private Set<User> participants = new HashSet<>();
 
+    @OneToMany(mappedBy = "schedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ScheduleItem> scheduleItems = new HashSet<>();
+
     @Column(name = "title", nullable = false, length = 50)
     private String title;
 
@@ -37,4 +59,7 @@ public class Schedule {
     @Column(name = "end_date")
     private LocalDate endDate;
 
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
