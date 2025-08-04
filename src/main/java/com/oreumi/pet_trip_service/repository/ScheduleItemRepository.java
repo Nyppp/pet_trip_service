b21005b07e0c834c155ac880d5bb1bde522a1425
Nyppp <wlsdnrla216@naver.com>
2025-08-04 10:14:25 +0900

    refactor: repository 어노테이션 누락 수정

--- src/main/java/com/oreumi/pet_trip_service/repository/ScheduleItemRepository.java
+++ src/main/java/com/oreumi/pet_trip_service/repository/ScheduleItemRepository.java
@@ -2,9 +2,11 @@ package com.oreumi.pet_trip_service.repository;
 
 import com.oreumi.pet_trip_service.model.ScheduleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ScheduleItemRepository extends JpaRepository<ScheduleItem, Long> {
     List<ScheduleItem> findAllByScheduleId(Long scheduleId);
     List<ScheduleItem> findAllByScheduleIdOrderByStartTime(Long scheduleId);
--- src/main/java/com/oreumi/pet_trip_service/repository/ScheduleRepository.java
+++ src/main/java/com/oreumi/pet_trip_service/repository/ScheduleRepository.java
@@ -4,10 +4,12 @@ import com.oreumi.pet_trip_service.model.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByUserId(Long userId);
     List<Schedule> findAllByUserIdOrderByStartDate(Long userId);
