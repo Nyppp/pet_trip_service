03c1dac1746957bedbb60e90a67575cf1aa55540
ksy <awers1010@gmail.com>
2025-08-04 10:23:14 +0900

    feat: ChatRepository, ChatRoomRepository 생성

--- src/main/java/com/oreumi/pet_trip_service/repository/ChatRepository.java
+++ src/main/java/com/oreumi/pet_trip_service/repository/ChatRepository.java
@@ -2,11 +2,16 @@ package com.oreumi.pet_trip_service.repository;
 
 import com.oreumi.pet_trip_service.model.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-    // 채팅방의 모든 채팅을 조회
-    List<Chat> findByChatRoomId(Long chatRoomId);
+    // 채팅방의 모든 채팅을 오름차순으로 조회
+    @Query(value = "SELECT * FROM chat WHERE chatroom_id = :chatRoomId ORDER BY created_at ASC", nativeQuery = true)
+    List<Chat> findByChatRoomIdAsc(@Param("chatRoomId")Long chatRoomId);
 }
