4ce2231e7ad4e1f946274add59c111f12cd07371
ksy <awers1010@gmail.com>
2025-08-04 10:36:02 +0900

    feat: ChatRepository의 findByChatRoomIdAsc의 조회기능을 자동맵핑으로 수정 -> findAllByChatRoomIdCreatedAt으로 메서드 변경

--- src/main/java/com/oreumi/pet_trip_service/repository/ChatRepository.java
+++ src/main/java/com/oreumi/pet_trip_service/repository/ChatRepository.java
@@ -2,7 +2,6 @@ package com.oreumi.pet_trip_service.repository;
 
 import com.oreumi.pet_trip_service.model.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +11,5 @@ import java.util.List;
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     // 채팅방의 모든 채팅을 오름차순으로 조회
-    @Query(value = "SELECT * FROM chat WHERE chatroom_id = :chatRoomId ORDER BY created_at ASC", nativeQuery = true)
-    List<Chat> findByChatRoomIdAsc(@Param("chatRoomId")Long chatRoomId);
+    List<Chat> findAllByChatRoomIdCreatedAt(@Param("chatRoomId")Long chatRoomId);
 }
