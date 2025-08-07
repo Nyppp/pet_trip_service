fbcfe2c3e58ca7dbe248bc42caa1d1ade3bd467a
jwLee <jwljwljwl777@gmail.com>
2025-08-07 13:41:50 +0900

    feat: place관련 controller 작성

---
+++ src/main/java/com/oreumi/pet_trip_service/controller/TourBatchController.java
@@ -0,0 +1,19 @@
+package com.oreumi.pet_trip_service.controller;
+
+import com.oreumi.pet_trip_service.service.PlaceBatchCollector;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/tour")
+@RequiredArgsConstructor
+public class TourBatchController {
+
+    private final PlaceBatchCollector placeBatchCollector;
+
+    @GetMapping("/collect-all")
+    public String collectAll() throws Exception {
+        placeBatchCollector.collectAll();
+        return "전국 전체 지역 + 전체 콘텐츠타입 데이터 수집 완료";
+    }
+}
