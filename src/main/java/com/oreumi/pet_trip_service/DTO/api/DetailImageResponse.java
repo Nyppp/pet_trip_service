093d029cccd0555803bb7d73f42fcdee6fe02965
jwLee <jwljwljwl777@gmail.com>
2025-08-07 13:42:06 +0900

    feat: api 관련 dto 작성

---
+++ src/main/java/com/oreumi/pet_trip_service/DTO/api/DetailImageResponse.java
@@ -0,0 +1,35 @@
+package com.oreumi.pet_trip_service.DTO.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DetailImageResponse {
+    private Response response;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Response {
+        private Body body;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Body {
+        private Items items;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Items {
+        private List<Item> item;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Item {
+        private String originimgurl;
+    }
+}
