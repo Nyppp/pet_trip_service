48e26f74330aa03b4a6d3af8d832d637ee8a6612
jwLee <jwljwljwl777@gmail.com>
2025-08-11 14:27:03 +0900

    refactor: api 패키지 구조 수정

--- src/main/java/com/oreumi/pet_trip_service/service/PlaceDataCollector.java
+++ src/main/java/com/oreumi/pet_trip_service/service/PlaceDataCollector.java
@@ -1,4 +1,4 @@
-package com.oreumi.pet_trip_service.service.api;
+package com.oreumi.pet_trip_service.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
